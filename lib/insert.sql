14c1610e13534fce71ea4775e5625504dffbf732
Andrew Williams <1williamsaj@gmail.com>
2019-11-18 00:05:03 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -31,10 +31,10 @@ INSERT INTO users (name, age) VALUES ("Zach Hale", 24);
 
 INSERT INTO pledges (amount, user_id, project_id) VALUES (260, 1, 2);
 INSERT INTO pledges (amount, user_id, project_id) VALUES (460, 3, 5);
-INSERT INTO pledges (amount, user_id, project_id) VALUES (250, 7, 9);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (250, 7, 6);
 INSERT INTO pledges (amount, user_id, project_id) VALUES (560, 7, 8);
-INSERT INTO pledges (amount, user_id, project_id) VALUES (320, 2, 4);
-INSERT INTO pledges (amount, user_id, project_id) VALUES (50, 3, 8);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (320, 4, 3);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (450, 3, 8);
 INSERT INTO pledges (amount, user_id, project_id) VALUES (50, 3, 1);
 INSERT INTO pledges (amount, user_id, project_id) VALUES (50, 4, 2);
 INSERT INTO pledges (amount, user_id, project_id) VALUES (50, 5, 3);
