a2fc50352ae46e60820523d7cc043fe3e3848f86
Andrew Williams <1williamsaj@gmail.com>
2019-11-18 00:35:28 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,7 +23,7 @@ end
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
   "SELECT projects.title, (projects.funding_goal - SUM(pledges.amount)) * -1 AS over FROM projects
    INNER JOIN pledges
-   ON project_id = pledges.project_id
+   ON projects.id = pledges.project_id
    GROUP BY projects.title
    HAVING over >= 0;"
 end
