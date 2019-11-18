341e37c6de7c848ad0ad3430ab9c7887080f1713
Andrew Williams <1williamsaj@gmail.com>
2019-11-18 00:13:22 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -7,7 +7,7 @@ INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUE
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Hungry for Houston", "Homeless", 2000, "1/5/20", "1/31/20");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Wounded Warriors", "Military", 250, "3/2/19", "5/15/20");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Voice of the Silent", "Human Trafficking", 5000, "9/14/19", "10/02/19");
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Town Hall Restoration, "Community")
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Town Hall Restoration", "Community", 50000, "2/1/20", "10/1/20")
 
 INSERT INTO users (name, age) VALUES ("Bailee Williams", 36);
 INSERT INTO users (name, age) VALUES ("Jacob Zeltman", 23);
